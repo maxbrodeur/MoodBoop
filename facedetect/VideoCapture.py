ebeff8262ec3f7c53fe391dabaf1448ae3cdbecf
dbreton333 <davidbreton03@gmail.com>
2021-02-06 14:04:53 -0500

    create video capture file

---
+++ facedetect/VideoCapture.py
@@ -0,0 +1,31 @@
+from time import time
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+img_counter = 0
+pastTime=0
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frame_counter= 0
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+    cv2.imshow('gray', gray)
+    k=cv2.waitKey(1)
+    newTime = int (time() % 60)
+    if  newTime!=pastTime:
+        # SPACE pressed
+        pastTime=newTime
+        img_name = "data\opencv_frame_{}.png".format(img_counter)
+        cv2.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
+        img_counter += 1
+    elif k & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+
+cap.release()
+cv2.destroyAllWindows()
