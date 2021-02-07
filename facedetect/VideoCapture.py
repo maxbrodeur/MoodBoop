982c5e291bed36bb7fe170fd11f25ce475c45e5f
Max Brodeur <maxbrod21@gmail.com>
2021-02-07 09:37:11 -0500

    AI WORKS BITCHES

--- facedetect/VideoCapture.py
+++ facedetect/VideoCapture.py
@@ -2,28 +2,29 @@ from time import time
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
-img_counter = 0
-pastTime = 0
-while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    frame_counter = 0
-    # Display the resulting frame
-    cv2.imshow('frame', frame)
-    k = cv2.waitKey(1)
-    newTime = int(time() % 60)
-    if newTime != pastTime:
-        # SPACE pressed
-        pastTime = newTime
-        img_name = "data/opencv_frame_{}.jpg".format(img_counter)
-        cv2.imwrite(img_name, frame)
-        print("{} written!".format(img_name))
-        img_counter += 1
-    elif k & 0xFF == ord('q'):
-        break
+class VideoCapture:
 
-# When everything done, release the capture
+    def capture(self):
+        cap = cv2.VideoCapture(0)
+        img_counter = 0
+        pastTime = 0
+        img = np.empty([10])
+        while img_counter!=10:
+            # Capture frame-by-frame
+            ret, frame = cap.read()
+            frame_counter = 0
+            # Display the resulting frame
+            cv2.imshow('frame', frame)
+            k = cv2.waitKey(1)
+            newTime = int(time() % 60)
+            if newTime != pastTime:
+                # SPACE pressed
+                pastTime = newTime
+                img[img_counter] = frame
+                img_counter += 1
 
-cap.release()
-cv2.destroyAllWindows()
+        # When everything done, release the capture
+
+        # cap.release()
+        # cv2.destroyAllWindows()
+        return img
