c0b93b05e1b0c6610d7a713b16b553f60be7e696
Max Brodeur <maxbrod21@gmail.com>
2021-02-07 03:00:22 -0500

    Started video conversions

--- videodata/prepare.py
+++ videodata/prepare.py
@@ -9,48 +9,10 @@ v2d = Vid2dat()
 path = "/Users/maxbrodeur/Documents/Training"
 
 
-# for videoPath in os.listdir(path+"Video_Train_Set"):
-#
-#     # path names
-#     txtPath = videoPath[:-4] + ".txt"
-#     absPath = path + "Annotations_Train_Set/" + txtPath
-#     absPathVid = path + "Video_Train_Set/" + videoPath
-#     print("Analyzing " + videoPath + " and " + txtPath + "...")
-#
-#     if os.path.isfile(absPath):
-#
-#         f = open(absPath, "r")
-#
-#         # first line is valence and arousal
-#         f.readline()
-#         # only analyzing videos with one person (one arousal one valence)
-#         if len(f.readline().split(',')) < 3:
-#             count = 1
-#             for x in f:
-#                 count = count + 1
-#
-#             print(str(count) + " frames...")
-#
-#             frames = v2d.vid2frames(absPathVid, count)
-#
-#             newDir = path + "NewData/Training/" +videoPath[:-4]
-#             os.mkdir(newDir)
-#             newDir2 = newDir+"/FRAMES"
-#             os.mkdir(newDir2)
-#
-#             print("filling " + newDir2 + " with " + str(len(frames)) + "...")
-#
-#             count = 1
-#             for frame in frames:
-#                 cv2.imshow("frame", frame)
-#                 cv2.waitKey(1)
-#                 cv2.imwrite(newDir2 + "/" + videoPath[:-4] + str(count) + ".jpg", frame)
-#                 count = count + 1
-#
-#             copyfile(absPath, newDir)
 
 
 def prepare(pathVids, pathAnnotations, newPath):
+
     for videoPath in os.listdir(pathVids):
 
         # path names
@@ -73,6 +35,7 @@ def prepare(pathVids, pathAnnotations, newPath):
 
                 print(str(count) + " frames...")
 
+                # Video to frames
                 frames = v2d.vid2frames(absPathVid, count)
 
                 newDir = newPath + "/" + videoPath[:-4]
